271361a946949ebc9a86ce7735a03d1f8d95d5ca
KIM <yjl1845@naver.com>
2023-11-08 21:41:27 +0900

    2023-11-08

---
+++ Assets/Script/NotificationManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationManager : MonoBehaviour
+{
+    [SerializeField] Text content;
+
+    public static void NotificationWindow(string message)
+    {
+        GameObject window = Instantiate(Resources.Load<GameObject>("Notification Window"));
+        window.GetComponent<NotificationManager>().content.text = message;
+    }
+
+    public void Close()
+    {
+        Destroy(gameObject);
+    }
+}
