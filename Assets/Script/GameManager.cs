335190b4edd79599fa004c50f38f47213dff28b7
KIM <yjl1845@naver.com>
2023-11-15 21:45:14 +0900

    2023-11-15

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject preasurBox;
+
+    public Vector3 RandomPosition(int value)
     {
-        
+        Vector3 direction = Random.insideUnitSphere;
+
+        direction *= value;
+
+        direction.y = 0;
+
+        return direction;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        PhotonNetwork.Instantiate("Character", RandomPosition(5), Quaternion.identity);
+    }
+
+    private IEnumerator CreateObject()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(5f);
+
+            PhotonNetwork.Instantiate("Treasure Box", RandomPosition(25), Quaternion.identity);
+        }
+    }
+
+    public void ExitGame()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    private void Start()
+    {
+        if(PhotonNetwork.IsMasterClient)
+        {
+            StartCoroutine(CreateObject());
+        }
+    }
+
+    public override void OnLeftRoom()
+    {
+        PhotonNetwork.LoadLevel("Photon Room");
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        PhotonNetwork.SetMasterClient(PhotonNetwork.PlayerList[0]);
     }
 }
